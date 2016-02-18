98889a325b59777d6f35412aa543d1fbf93f6a94
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2016-02-18 13:48:53 -0800

    apparently wget has some facy timestamp checking built in, so we will use that instead of my crazy hashing a web page method

--- autoplot.sh
+++ autoplot.sh
@@ -5,25 +5,10 @@ AUTOPLOT_HOME=$HOME/autoplot_data
 JARFILE=$AUTOPLOT_HOME/autoplot.jar;
 mkdir -p $AUTOPLOT_HOME/log
 
-#Download the webpage of the lasest version. This page contains the 
-#current version number.
-wget http://autoplot.org/jnlp/latest/index.html -O$AUTOPLOT_HOME/.html.tmp > /dev/null 2>/dev/null
-
-#Comparing a hash of the downloaded page to the hash of the previously
-#downloaded page will tell us if the version has changed.
-APVERHASH=$(md5sum $AUTOPLOT_HOME/.html.tmp | tr -d '[:space:]')
-APVERHASH_OLD=$(cat $AUTOPLOT_HOME/.versionhash | tr -d '[:space:]')
-
-if [ "$APVERHASH" != "$APVERHASH_OLD" ]; then
-   #Something (presumably the version number) has changed on the webpage. 
-   #Download a new copy of autoplot.jar
-   echo "Updating Autolplot..."
-   echo $APVERHASH > $AUTOPLOT_HOME/.versionhash
-   rm $AUTOPLOT_HOME/autoplot.jar
-   wget http://autoplot.org/jnlp/latest/autoplot.jar -O$AUTOPLOT_HOME/autoplot.jar
-fi
-#clean up the downloaded webpage
-rm $AUTOPLOT_HOME/.html.tmp*
+echo "Checking for Autolplot update..."
+cd $AUTOPLOT_HOME
+wget -N http://autoplot.org/jnlp/latest/autoplot.jar
+cd -
 
 #get any user supplied arguments
 JAVA_ARGS=""
