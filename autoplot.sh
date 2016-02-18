d4ffe6c06215bbd4c9f804228825a86af2ead320
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2016-02-18 13:17:32 -0800

    handles command line arguments for both java  in general and the jar file. Java arguments should be prefixed with -J

--- autoplot.sh
+++ autoplot.sh
@@ -2,6 +2,7 @@
 
 #make sure there is a valid place to keep autoplot and log info
 AUTOPLOT_HOME=$HOME/autoplot_data
+JARFILE=$AUTOPLOT_HOME/autoplot.jar;
 mkdir -p $AUTOPLOT_HOME/log
 
 #Download the webpage of the lasest version. This page contains the 
@@ -24,4 +25,41 @@ fi
 #clean up the downloaded webpage
 rm $AUTOPLOT_HOME/.html.tmp*
 
-java -jar $AUTOPLOT_HOME/autoplot.jar >$AUTOPLOT_HOME/log/out.txt 2>$AUTOPLOT_HOME/log/err.txt &
+#get any user supplied arguments
+JAVA_ARGS=""
+AP_ARGS=""
+memIsImplicit=1
+
+for i in "$@"; do
+   if [ "$APDEBUG" == "1" ]; then    
+       echo "arg: \"$i\""
+   fi
+   if [[ $i == -J-Xmx* ]]; then
+      JAVA_ARGS="${JAVA_ARGS} ${i:2}";
+      memIsImplicit=0
+   elif [[ $i == -J* ]]; then
+      JAVA_ARGS="${JAVA_ARGS} ${i:2}";
+   elif [[ $i == '--headless' ]]; then
+      JAVA_ARGS="${JAVA_ARGS} -Djava.awt.headless=true";
+   elif [[ $i == '-h' ]]; then
+      JAVA_ARGS="${JAVA_ARGS} -Djava.awt.headless=true";
+   else
+      AP_ARGS="${AP_ARGS} $i";
+   fi
+done
+
+#Try to run java as specified by $JAVA_HOME. If that variable isn't set, just
+#use whatever java shows up first in the PATH 
+if [ "$APDEBUG" == "1" ]; then 
+   if [ "${JAVA_HOME}" -a \( -x "${JAVA_HOME}"/bin/java \) ]; then      
+      echo $EXEC "${JAVA_HOME}"/bin/java ${JAVA_ARGS} -jar ${JARFILE} "${AP_ARGS}"
+   else
+      echo $EXEC /usr/bin/env java ${JAVA_ARGS} -jar ${JARFILE} "${AP_ARGS}"
+   fi
+fi
+
+if [ "${JAVA_HOME}" -a \( -x "${JAVA_HOME}"/bin/java \) ]; then      
+      $EXEC "${JAVA_HOME}"/bin/java ${JAVA_ARGS} -jar ${JARFILE} "${AP_ARGS}" >$AUTOPLOT_HOME/log/out.txt 2>$AUTOPLOT_HOME/log/err.txt &
+else
+      $EXEC /usr/bin/env java ${JAVA_ARGS} -jar ${JARFILE} "${AP_ARGS}" >$AUTOPLOT_HOME/log/out.txt 2>$AUTOPLOT_HOME/log/err.txt &
+fi
