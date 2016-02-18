c9d3c9239d7379afa2779e3cd69e4a04cf00753c
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2016-02-18 12:51:58 -0800

    added some explainations about what is going on. Made the variables more bashy. Stop storing autplot.index in /tmp

--- autoplot.sh
+++ autoplot.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-mkdir -p $HOME/autoplot_data/log
-wget http://autoplot.org/jnlp/latest/index.html -O/tmp/autoplot.index > /dev/null 2>/dev/null
-autoplotversionhash=$(cat /tmp/autoplot.index | md5sum | tr -d '[:space:]')
-oldautoplotversionhash=$(cat $HOME/autoplot_data/.versionhash | tr -d '[:space:]')
-if [ "$autoplotversionhash" != "$oldautoplotversionhash" ]; then
+
+#make sure there is a valid place to keep autoplot and log info
+AUTOPLOT_HOME=$HOME/autoplot_data
+mkdir -p $AUTOPLOT_HOME/log
+
+#Download the webpage of the lasest version. This page contains the 
+#current version number.
+wget http://autoplot.org/jnlp/latest/index.html -O$AUTOPLOT_HOME/.html.tmp > /dev/null 2>/dev/null
+
+#Comparing a hash of the downloaded page to the hash of the previously
+#downloaded page will tell us if the version has changed.
+APVERHASH=$(md5sum $AUTOPLOT_HOME/.html.tmp | tr -d '[:space:]')
+APVERHASH_OLD=$(cat $AUTOPLOT_HOME/.versionhash | tr -d '[:space:]')
+if [ "$APVERHASH" != "$APVERHASH_OLD" ]; then
+   #Something (presumably the version number) has changed on the webpage. 
+   #Download a new copy of autoplot.jar
    echo "Updating Autolplot..."
-   echo $autoplotversionhash > $HOME/autoplot_data/.versionhash
-   rm $HOME/autoplot_data/autoplot.jar
-   wget http://autoplot.org/jnlp/latest/autoplot.jar -O$HOME/autoplot_data/autoplot.jar
+   echo $autoplotversionhash > $AUTOPLOT_HOME/.versionhash
+   rm $AUTOPLOT_HOME/autoplot.jar
+   wget http://autoplot.org/jnlp/latest/autoplot.jar -O$AUTOPLOT_HOME/autoplot.jar
 fi
+#clean up the downloaded webpage
+rm $AUTOPLOT_HOME/.html.tmp*
 
-rm /tmp/autoplot.index*
-
-java -jar $HOME/autoplot_data/autoplot.jar >$HOME/autoplot_data/log/out.txt 2>$HOME/autoplot_data/log/err.txt &
+java -jar $AUTOPLOT_HOME/autoplot.jar >$AUTOPLOT_HOME/log/out.txt 2>$AUTOPLOT_HOME/log/err.txt &
