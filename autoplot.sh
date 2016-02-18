a53298f12a771ff96f629a629e6acc6b5ad52e63
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2016-02-18 13:00:44 -0800

    fixed a bad variable

--- autoplot.sh
+++ autoplot.sh
@@ -12,11 +12,12 @@ wget http://autoplot.org/jnlp/latest/index.html -O$AUTOPLOT_HOME/.html.tmp > /de
 #downloaded page will tell us if the version has changed.
 APVERHASH=$(md5sum $AUTOPLOT_HOME/.html.tmp | tr -d '[:space:]')
 APVERHASH_OLD=$(cat $AUTOPLOT_HOME/.versionhash | tr -d '[:space:]')
+
 if [ "$APVERHASH" != "$APVERHASH_OLD" ]; then
    #Something (presumably the version number) has changed on the webpage. 
    #Download a new copy of autoplot.jar
    echo "Updating Autolplot..."
-   echo $autoplotversionhash > $AUTOPLOT_HOME/.versionhash
+   echo $APVERHASH > $AUTOPLOT_HOME/.versionhash
    rm $AUTOPLOT_HOME/autoplot.jar
    wget http://autoplot.org/jnlp/latest/autoplot.jar -O$AUTOPLOT_HOME/autoplot.jar
 fi
