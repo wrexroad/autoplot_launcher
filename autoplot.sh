74f3f43a8e9de6e7aa3a82ec5703c10713698c9d
Warren Rexroad <wrexroad@ssl.berkeley.edu>
2016-02-19 16:12:08 -0800

    added install options which generates a .desktop file

--- autoplot.sh
+++ autoplot.sh
@@ -8,8 +8,63 @@ AP_ARGS=""
 AP_VER="latest"
 memIsImplicit=1
 
+function linuxinstall {
+   DESKTOPFILE="$HOME/.local/share/applications/autoplot.desktop"
+   ICONFILE="$AP_HOME/autoplot.png"
+   SCRIPTFILE="$AP_HOME/autoplot.sh"
+   mkdir -p ~/.local/share/applications/
+   mkdir -p $AP_HOME
+   
+   #copy all files to the $AP_HOME
+   cp autoplot.sh $AP_HOME
+   chmod a+x $AP_HOME/autoplot.sh
+   cp autoplot.png $ICONFILE
+      
+   touch $DESKTOPFILE
+   echo "[Desktop Entry]" > $DESKTOPFILE
+   echo "Name=Autoplot" >> $DESKTOPFILE
+   echo "Exec=$SCRIPTFILE" >> $DESKTOPFILE
+   echo "Icon=$ICONFILE" >> $DESKTOPFILE
+   echo "Type=Application" >> $DESKTOPFILE
+   echo "Terminal=true" >> $DESKTOPFILE
+   echo "Categories=Graphics;Science;Utility;" >> $DESKTOPFILE
+
+   #try to link the script to a location in the user's PATH
+   SETPATH=0
+   array=(${PATH//:/ })
+   for i in "${array[@]}"
+   do
+      ln -s $SCRIPTFILE $i/autoplot
+      if [ "$?" == "0" ]; then
+         SETPATH=1
+         break
+      fi
+   done
+   
+   echo ""
+   echo "---------------------------"
+   echo ""
+   echo "Autoplot installed!"
+   echo "autoplot.sh (this file) located: $SCRIPTFILE"
+   echo "autoplot.svg (icon file) located: $ICONFILE"
+   echo "autoplot.desktop located: $DESKTOPFILE"
+   if [ "$SETPATH" == "0" ]; then
+      echo "Could not link $SCRIPTFILE to your PATH."
+      echo "To run Autoplot from the command line you will need to specify the full path."
+      echo ""
+      echo "Type \"$SCRIPTFILE --help\" for options."
+   else
+      echo "Linked autoplot.sh to your path as 'autoplot'."
+      echo ""
+      echo "Type \"autoplot --help\" for options."  
+   fi
+   echo ""
+   echo "---------------------------"
+   echo ""
+}
+
 function printhelp {
-   echo "Autoplot Launcher rev160219"
+   echo "Autoplot Launcher"
    echo
    echo "usage: ./autoplot.sh [options]"
    echo
@@ -19,11 +74,11 @@ function printhelp {
    echo "-J-{javaopts}             Options for the Java Virtual Machine should"
    echo "                          be prefixed with '-J'."
    echo "-h, --headless            Run Autoplot in headless mode."
+   echo "--install                 Creates an launcer icon. Linux Only."
    echo "--debug                   Autoplot Launcher mode provides extra dialog"
    echo "                          about what is happening in this script."
    echo "--help                    Prints this dialog."
    echo
-   exit
 }
 
 function getjars {
@@ -113,8 +168,12 @@ for i in "$@"; do
       AP_VER="${i:10}";
    elif [[ $i == "--debug" ]]; then
       APDEBUG=1;
+   elif [[ $i == "--install" ]]; then
+      linuxinstall
+      exit
    elif [[ $i == "--help" ]]; then
-      printhelp;
+      printhelp
+      exit
    else
       AP_ARGS="${AP_ARGS} $i";
    fi
@@ -138,19 +197,18 @@ cd -
 #Try to run java as specified by $JAVA_HOME. If that variable isn't set, just
 #use whatever java shows up first in $PATH 
 echo "Starting Autoplot..."
+echo "Log files are being stored in $AP_LOG/out.txt and $AP_LOG/err.txt"
 
 if [ "$APDEBUG" == "1" ]; then
    if [ "${JAVA_HOME}" -a \( -x "${JAVA_HOME}"/bin/java \) ]; then
-      echo "[DEBUG]" $EXEC "${JAVA_HOME}"/bin/java -cp ${AP_LIB}/AutoplotVolatile.jar:$AP_LIB/$AP_STAB ${JAVA_ARGS} $MAINCLASS "${AP_ARGS}"
+      echo "[DEBUG]" $EXEC nohup "${JAVA_HOME}"/bin/java -cp ${AP_LIB}/AutoplotVolatile.jar:$AP_LIB/$AP_STAB ${JAVA_ARGS} $MAINCLASS "${AP_ARGS}"
    else
-      echo "[DEBUG]" $EXEC /usr/bin/env java -cp ${AP_LIB}/AutoplotVolatile.jar:$AP_LIB/$AP_STAB ${JAVA_ARGS} $MAINCLASS "${AP_ARGS}"
+      echo "[DEBUG]" $EXEC nohup /usr/bin/env java -cp ${AP_LIB}/AutoplotVolatile.jar:$AP_LIB/$AP_STAB ${JAVA_ARGS} $MAINCLASS "${AP_ARGS}"
    fi
 fi
 
 if [ "${JAVA_HOME}" -a \( -x "${JAVA_HOME}"/bin/java \) ]; then
-      $EXEC "${JAVA_HOME}"/bin/java -cp ${AP_LIB}/AutoplotVolatile.jar:$AP_LIB/$AP_STAB ${JAVA_ARGS} $MAINCLASS "${AP_ARGS}" >$AP_HOME/log/out.txt 2>$AP_HOME/log/err.txt &
+      nohup $EXEC "${JAVA_HOME}"/bin/java -cp ${AP_LIB}/AutoplotVolatile.jar:$AP_LIB/$AP_STAB ${JAVA_ARGS} $MAINCLASS "${AP_ARGS}" >$AP_HOME/log/out.txt 2>$AP_HOME/log/err.txt
 else
-      $EXEC /usr/bin/env java -cp ${AP_LIB}/AutoplotVolatile.jar:$AP_LIB/$AP_STAB ${JAVA_ARGS} $MAINCLASS "${AP_ARGS}" >$AP_HOME/log/out.txt 2>$AP_HOME/log/err.txt &
+      nohup $EXEC /usr/bin/env java -cp ${AP_LIB}/AutoplotVolatile.jar:$AP_LIB/$AP_STAB ${JAVA_ARGS} $MAINCLASS "${AP_ARGS}" >$AP_HOME/log/out.txt 2>$AP_HOME/log/err.txt
 fi
-
-echo "Log files are being stored in $AP_LOG/out.txt and $AP_LOG/err.txt"
